85beaff00dbe2524867028c7f8e0c0b738af647f
d s <secondoneaddress@outlook.com>
2021-04-03 14:25:27 +0200

    erste vollstädige comment-skelette von post.py und get.py ; neu: allgemeine technische zeichnung

--- get.py
+++ get.py
@@ -3,5 +3,16 @@
 #variablen der messdaten abrufen
 
 """
-#variablen 
+#get anfrage für die sensordaten stellen
+# ggf. aus textdatei abrufen?
+# 
+# daten in variablen schreiben (Feuchte_PF4 for example) 
+
+# anfrage für gesetzte schwellwerte (webbasiert vorgegeben wenn unter ... feuchtigkeit = wässern)
+# stehen ggf. bereits in variable?
+# alternativ: in variable schreiben
+
+# abgleichung, liegt messwert über schwellwert
+# ja --> alles wie bisher, KEIN Wasser
+# nein --> wasser marsch!
 """
--- post.py
+++ post.py
@@ -1,3 +1,12 @@
 #script1
 
 #sensordaten in variablen schreiben
+
+"""
+# GPIOs als input definieren
+# sensoren benennen? (GPIO2 = Pflanze1, etc.)
+# senordaten abrufen
+# in variable schreiben (messwert_PF1)
+# in textdatei speichern?
+# in irgendeiner form posten
+"""
